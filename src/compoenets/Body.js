3b5398caf442ed070b8f3865506f959680781628
swapnil1099 <67257402+swapnil1099@users.noreply.github.com>
2024-02-24 18:49:57 +0530

    search button functionlity

--- src/compoenets/Body.js
+++ src/compoenets/Body.js
@@ -6,32 +6,48 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
     const [newresList, setnewresList] = useState([]);
-    useEffect(()=>{
+    const [searchText, SetSearchText] = useState("");
+    const [list,setList]=useState([]);
+    useEffect(() => {
         fetchData();
-    },[]);
+    }, []);
 
 
-   const fetchData=async()=>{
-    const data=await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=18.5204303&lng=73.8567437")
-    const json=await data.json();
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=18.5204303&lng=73.8567437")
+        const json = await data.json();
+        setList(json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+        setnewresList(json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
 
-    setnewresList(json.data.success.cards[1].gridWidget.gridElements.infoWithStyle.restaurants);
-   
-   }
+    }
 
 
-    
-    return newresList.length===0? <Shimmer/>: (
+
+    return newresList.length === 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
-                <button className="filter-res" onClick={() => {
-                    const filterList = newresList.filter((rest) => (rest.info.avgRating > 4))
-                    setnewresList(filterList);
-                }}>
+                <div className="search-box">
+                    <input className="search-box" value={searchText} onChange={(e) => {
+                        SetSearchText(e.target.value)
+                    }} />
+                    <button className="search-bt" onClick={() => {
+                        const searchlist = list.filter((res1) => (res1.info.name.toLowerCase().includes(searchText.toLowerCase())));
+                        setnewresList(searchlist);
 
-                    Top Rated Resto
+                    }}>search</button>
 
-                </button>
+                </div>
+                <div className="filter-div">
+                    <button className="filter-res" onClick={() => {
+                        const filterList = newresList.filter((rest) => (rest.info.avgRating > 4))
+                        setnewresList(filterList);
+                    }}>
+
+                        Top Rated Resto
+
+                    </button>
+
+                </div>
             </div>
             <div className="res-container">
 
