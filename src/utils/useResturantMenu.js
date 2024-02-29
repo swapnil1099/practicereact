2335626fb7c3c81cad05171ffe8c8b6060f20a36
swapnil1099 <67257402+swapnil1099@users.noreply.github.com>
2024-02-29 11:31:01 +0530

    updated URL new one 1

--- src/utils/useResturantMenu.js
+++ src/utils/useResturantMenu.js
@@ -9,6 +9,7 @@ const useResturantMenu = (resId) => {
   async function fetchMenu() {
     const datajson = await fetch("https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5504303&lng=73.8567437&restaurantId=" + resId);
 
+    
     const data = await datajson.json();
 
 
